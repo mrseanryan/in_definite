6dd0488384992c819267c5beede2c176371572c2
sean <sean.ryan@mendix.com>
2019-09-25 13:50:31 +0200

    Add cargo/bin to the PATH env variable

--- install-cargo-release.sh
+++ install-cargo-release.sh
@@ -1,1 +1,6 @@
 cargo install cargo-release
+
+echo "Adding cargo to the \$PATH variable"
+echo export PATH="\$HOME/.cargo/bin:\$PATH" >> $HOME/.bashrc
+echo "To add cargo to *this* terminal's PATH, type this:"
+echo "source \$HOME/.bashrc"
